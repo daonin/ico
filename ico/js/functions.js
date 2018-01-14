11a8f24194f858a728b949491a67c531e68a232e
daonin <alex.koskin@gmail.com>
2018-01-14 20:56:04 +0300

    New fonts. New bgrnd. Multilanguage

---
+++ ico/js/functions.js
@@ -0,0 +1,32 @@
+﻿$(function () {
+    $(".custom-select").each(function() {
+        function e(n) {
+            n.removeClass("custom-select--open")
+        }
+        var n = $(this),
+            o = $(".custom-select"),
+            i = n.find(".custom-select__placeholder"),
+            r = n.find(".custom-select__items");
+        n.on("click", function(r) {
+            if (r.target === n[0] || r.target === i[0])
+                if (n.hasClass("custom-select--open")) n.removeClass("custom-select--open");
+                else {
+                    r.stopPropagation();
+                    for (var a = 0, s = o.not(this), c = s.length; a < c; a++) s[a].classList.remove("custom-select--open");
+                    n.addClass("custom-select--open"), $("body").one("click", e)
+                }
+        }), r.on("click", ".custom-select__item", function() {
+            var o = r.find('label[for="' + $(this).attr("id") + '"]').text();
+            i.html(o), n.removeClass("custom-select--open"), $("body").off("click", e);
+            var lang = $(this).attr("id");
+                        
+            $('.main-logo').attr('src', arrLang[lang]['logo-src']);
+            
+
+            $('.lang').each(function (index, element) {
+                $(this).text(arrLang[lang][$(this).attr('key')]);
+            })
+
+        })
+    })
+});
---
+++ ico/js/translation.js
@@ -0,0 +1,38 @@
+﻿var arrLang = {
+    'en': {
+        'logo-src': '/images/header-logo--international.svg',
+
+        'menu_about': 'About',
+        'menu_ico': 'ICO',
+        'menu_contacts': 'Contacts',
+        'menu_whitepaper': 'WhitePaper',
+
+        'conuntdown_days': 'days',
+        'conuntdown_hours': 'hours',
+        'conuntdown_mins': 'minutes',
+        'conuntdown_secs': 'seconds',
+
+        'intro-title-1': 'Decentralised platform',
+        'intro-title-2': 'for brain-fitness',
+        'intro-presale-finish': 'Presale ends in',
+        'btn_participate': 'Participate'
+    },
+    'ru': {
+        'logo-src': '/images/header-logo.svg',
+
+        'menu_about': 'О проекте',
+        'menu_ico': 'ICO',
+        'menu_contacts': 'Контакты',
+        'menu_whitepaper': 'Белая Бумага',
+
+        'conuntdown_days': 'дни',
+        'conuntdown_hours': 'часы',
+        'conuntdown_mins': 'минуты',
+        'conuntdown_secs': 'секунды',
+
+        'intro-title-1': 'Децентрализованная платформа',
+        'intro-title-2': 'для тренировки мозга',
+        'intro-presale-finish': 'До конца пресейла осталось',
+        'btn_participate': 'Участвовать'
+    }
+}
